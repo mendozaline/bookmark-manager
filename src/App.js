8c853c43c8c6f10098fead8d3026ab175383122a
Andrew Brennwald <Ando@CloudCity.local>
2017-02-01 19:48:05 -0800

    Allow multiple resource in state.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,23 @@ class App extends Component {
     super();
 
     this.state = {
-      resource: "https://facebook.github.io/react/docs/thinking-in-react.html"
+      resources: [
+        {title: "Thinking in React",
+         url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
+        {title: "Mindspace React Tutorial",
+         url: "https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"},
+        {title: "LearnCode Academy React Tutorial",
+         url: "https://youtu.be/fd2Cayhez58"}]
     };
   }
 
-// event handlers here
-
   render() {
     return (
-      <a href={this.state.resource}>Thinking in React</a>
+      <div>
+        {this.state.resources.map((resource) => {
+          return <a href={resource.url}>{resource.title}</a>
+        })}
+      </div>
     );
   }
 }
