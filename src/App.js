a16b7314e89b3c4679e3b96e634cb8009c0bb1ff
Andrew Brennwald <Ando@CloudCity.local>
2017-02-01 20:04:22 -0800

    Added more resources.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ class App extends Component {
 
     this.state = {
       resources: [
+        {title: "FunFunFunction Functional Programming Playlist",
+         url: "https://www.youtube.com/watch?v=BMUiFMZr7vk&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84"},
+        {title: "FunFunFunction var, let, and const",
+         url: "https://www.youtube.com/watch?v=sjyJBL5fkp8"},
+        {title: "FunFunFunction arrow functions",
+         url: "https://www.youtube.com/watch?v=6sQDTgOqh-I"},
+        {title: "Kyle Robinson Young ES6 Essentials",
+         url: "https://www.youtube.com/watch?v=CozSF5abcTA"},
         {title: "Thinking in React",
          url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
         {title: "Mindspace React Tutorial",
