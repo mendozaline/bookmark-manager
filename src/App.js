aeedc8605f57ea9b782b044bf8de4bf5f0c7909a
Andrew Brennwald <Ando@CloudCity.local>
2017-02-01 20:33:23 -0800

    Updated state schema for subjects. FEATURE NOT FINISHED.

--- src/App.js
+++ src/App.js
@@ -6,23 +6,55 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = {
-      resources: [
-        {title: "FunFunFunction Functional Programming Playlist",
-         url: "https://www.youtube.com/watch?v=BMUiFMZr7vk&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84"},
-        {title: "FunFunFunction var, let, and const",
-         url: "https://www.youtube.com/watch?v=sjyJBL5fkp8"},
-        {title: "FunFunFunction arrow functions",
-         url: "https://www.youtube.com/watch?v=6sQDTgOqh-I"},
-        {title: "Kyle Robinson Young ES6 Essentials",
-         url: "https://www.youtube.com/watch?v=CozSF5abcTA"},
-        {title: "Thinking in React",
-         url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
-        {title: "Mindspace React Tutorial",
-         url: "https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"},
-        {title: "LearnCode Academy React Tutorial",
-         url: "https://youtu.be/fd2Cayhez58"}]
-    };
+    this.state = {resources: [
+
+      {
+        subject: "Functional Programming Basics",
+        resources: [
+          {
+            title: "FunFunFunction Functional Programming Playlist",
+            url: "https://www.youtube.com/watch?v=BMUiFMZr7vk&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84"
+          },
+          {
+            title: "FunFunFunction var, let, and const",
+            url: "https://www.youtube.com/watch?v=sjyJBL5fkp8"
+          },
+          {
+            title: "FunFunFunction arrow functions",
+            url: "https://www.youtube.com/watch?v=6sQDTgOqh-I"
+          }
+        ]
+      },
+
+
+      {
+        subject: "ES6",
+        resources: [
+          {
+            title: "Kyle Robinson Young ES6 Essentials",
+            url: "https://www.youtube.com/watch?v=CozSF5abcTA"
+          }
+        ]
+      },
+
+
+      {
+        subject: "Intro to React",
+        resources: [
+          {
+            title: "Thinking in React",
+            url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
+          {
+            title: "Mindspace React Tutorial",
+            url: "https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"},
+          {
+            title: "LearnCode Academy React Tutorial",
+            url: "https://youtu.be/fd2Cayhez58"
+          }
+        ]
+      }
+
+    ]}
   }
 
   render() {
