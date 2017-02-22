a05460b514d5460e7d558033dc12574e77b95ccd
David Mendoza <mendoza.line@aim.com>
2017-02-21 21:47:30 -0800

    Implement drop-down menu for video playlists

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -4,61 +4,110 @@ export default class Subject extends Component {
   constructor(props) {
     super(props)
     // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick = this.handleClick.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleTyping = this.handleTyping.bind(this)
+    this.dropDown = this.dropDown.bind(this)
+    this.dropDownSub = this.dropDownSub.bind(this)
 
     this.state = {
       isClicked: false,
-      isClicked2: false
+      playlist: false,
+      dropdownUrl: ''
     }
-  }
+  } //end constructor
 
   handleClick() {
     this.setState(prevState => ({
       isClicked: !prevState.isClicked
-    }) );
-
-    console.log(this.state.isClicked);
-
+    }));
   }
 
-  handleClick2() {
-    this.setState(prevState => ({
-      isClicked2: !prevState.isClicked2
-    }) );
+  handleTyping(event) {
+    const target = event.target
+    const name = target.name
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    //ternary
+    console.log("primary name: ", name)
+    console.log("primary value: ", value)
 
-    console.log(this.state.isClicked2);
+    this.setState({
+      [name]: value
+    })
+//    console.log("title: ", this.state.title)
+//    console.log("url: ", this.state.url)
+//    console.log("playlist boolean: ", this.state.playlist)
+  }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('STATE:' +
+                '\ntitle: ' +  this.state.title + 
+                '\nurl: ' + this.state.url + 
+                '\nplaylist: ' + this.state.playlist + 
+                '\nlinks', this.state.links)
+
+    const newRes = {
+      title: this.state.title,
+      url: this.state.url,
+      playlist: this.state.playlist,
+      links: this.state.links
+    }
+    this.props.addRes(this.props.i, newRes)
+    this.setState({
+      title: '',
+      url: '',
+      playlist: false, //needs to switch back to false/unchecked
+                        //!this.state.playlist
+      playlistTitle: '',
+      playlistUrl: '',
+      links: [],
+    });
+  }
+
+  dropDown(event) {
+    event.preventDefault()
+    console.log('dropdown value: ', event.target.value)
+    this.setState({
+      dropdownUrl: event.target.value
+    })
+  }
+
+  dropDownSub(event) {
+    event.preventDefault()
+    window.open(this.state.dropdownUrl)
   }
 
   render() {
-    console.log("p1: ", this.props)
-    console.log("p2: ", this.props.subject)
-    console.log("p3: ", this.props.subject.topics)
-    console.log("p4: ", this.props.subject.topics[0].playlist)
 
     let resources = null
 
     let linksOrTitle = this.props.subject.topics.map((t, i) => {
       if (t.playlist) {
-        return  <div key={i}>
-                  <p onClick={this.handleClick2}>{t.title}</p>
-                  <ul>
-                      {t.links.map((l, j) => {
-                        if (this.state.isClicked2) {
-                          return  <li key={j}>
-                                    <a href={l.url}>{l.title}</a>
-                                  </li>
-                        } else {
-                          return null
-                        }
-                      }) }
-                    </ul>
-                </div>
+        return  <li key={i}>
+                  {t.title}
+                  <br/>
+                  <form onSubmit={this.dropDownSub}>
+                    <label>
+                      Pick your video:
+                      <select value={this.state.selected} onChange={this.dropDown}>
+                        <option value="">Select option</option>
+                          {t.links.map((l, j) => {
+                            return  <option key={j} value={l.url}>
+                                      {l.title}
+                                    </option>
+                          }) }
+                      </select>
+                    </label>
+                    <input type="submit" value="Submit" />
+                  </form>
+                  <p></p>
+                </li>
       } else {
-        return  <p key={i}>
+        return  <li key={i}>
                   <a href={t.url}>{t.title}</a>
-                </p>
+                  <p></p>
+                </li>
       }
     })
 
@@ -73,7 +122,11 @@ export default class Subject extends Component {
         <h2 onClick={this.handleClick}>
           {this.props.subject.subject}
         </h2>
-        {resources}
+
+        <ul>
+          {resources}
+        </ul>
+
       </div>
     )
   }
