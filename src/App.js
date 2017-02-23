da889d3407016d569e40d017139716bf91eb1c9a
David Mendoza <mendoza.line@aim.com>
2017-02-22 19:53:21 -0800

    Refactor add subject into its own component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Subject from './components/Subject.js';
+import AddSubject from './components/AddSubject.js';
 
 class App extends Component {
   constructor() {
     super();
 
     this.addNewRes = this.addNewRes.bind(this)
-    this.handleTyping = this.handleTyping.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.addNewSub = this.addNewSub.bind(this)
 
     this.state = {
       subject: '',
@@ -292,6 +292,7 @@ class App extends Component {
   }
 
   addNewRes(subject, resource) {
+    console.log('state: ', this.state)
     const tempState = this.state
     tempState.resources[subject].topics.push(resource)
     console.log('resources: ', this.state.resources)
@@ -299,28 +300,11 @@ class App extends Component {
     this.setState(tempState)
   }
 
-  handleTyping(event) {
-//    console.log('name: ', event.target.name)
-//    console.log('value: ', event.target.value)
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-//    console.log("subject: ", this.state.subject)
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-//    console.log('STATE:' + '\nsubject: ' +  this.state.subject)
-    const newSubject = {
-      subject: this.state.subject,
-      topics: [],
-    }
-
-    console.log('b: ', this.state.resources)
+  addNewSub(subject) {
     const tempSt = this.state
-    tempSt.resources.push(newSubject)
+    tempSt.resources.push(subject)
     this.setState(tempSt)
-    console.log('a: ', this.state.resources)
+    console.log('a: ', this.state)
   }
 
   render() {
@@ -328,21 +312,7 @@ class App extends Component {
 
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="subject">Add subject:</label>
-          <input
-            name="subject"
-            id="subject"
-            type="string"
-            value={this.state.subject}
-            onChange={this.handleTyping} />
-
-          <br />
-
-          <button onClick={this.handleSubmit}>Submit</button>
-        </form>
-
-        <br />
+        <AddSubject addSub={this.addNewSub}/>
 
         {
           this.state.resources.map((r, i) => {
